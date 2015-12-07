d64d4cfaa7bae1c0b5d1b3c8c62ea254482d3c2c
Colin McCulloch <mccolane@gmail.com>
2015-12-07 17:39:31 +0000

    Params and Random

--- app.rb
+++ app.rb
@@ -9,9 +9,13 @@ get '/rude_pictures' do
   "Sucker!"
 end
 
-get '/cats' do
-  "<div style='border: 3px dashed red; width:1500px;'>
-     <img src='http://bit.ly/1eze8aE'style='width:1500px;height:1500px;'>
-   </div>"
+get '/random-cat' do
+  @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
 
+get '/named-cat' do
+  p(params)
+  @name = params[:name]
+  erb(:index)
 end
